ad0cc2fa80365e799bd962e08d3d10d76a7b94f0
Arista Indrajaya <arista.indrajaya@gmail.com>
2020-09-17 12:31:53 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Juli - Blop
+Blockchain Micropayment - Improving Transaction Throughput
+Project workspace to develop integration between:
+1. Semantic2019
+2. HSG88/Payment-Channel -> refactor to Juli-Blop
+3. Additional feature with:
+a. GO-Perun and 
+b. Blockchain API
+
+![Blop Integration View](docs/blops-integration-v2.png?raw=true "Integration View")
---
+++ blockchain/Smart-Contract/contracts/PayMerkleExtended.sol
@@ -0,0 +1,45 @@
+pragma solidity >=0.4.22 <0.6.0;
+
+contract PayMerkleExtended {
+    address payable public channelSender;
+    address payable public channelRecipient;
+    uint public startDate;
+    uint public channelTimeout;
+    bytes32 public roots;
+
+    constructor (address payable to, uint _timeout, bytes32 _root) public payable {
+        require(msg.value>0);
+        channelRecipient = to;
+        channelSender = msg.sender;
+        startDate = now;
+        channelTimeout = _timeout;
+        roots = _root;
+    }
+    function AddBalance(bytes32 _newRoot) public payable {
+        if (roots < _newRoot)
+            roots = keccak256(abi.encodePacked(roots, _newRoot));
+        else
+            roots = keccak256(abi.encodePacked(_newRoot, roots));
+    }
+    function CloseChannel(uint256 _amount, bytes32[] memory proof) public  {
+        require(msg.sender==channelRecipient);
+        bytes32 computedHash = keccak256(abi.encodePacked(_amount));
+        require(verifyMerkle(roots, computedHash, proof));
+        channelRecipient.transfer(_amount);
+        selfdestruct(channelSender);
+    }
+    function verifyMerkle (bytes32 root, bytes32 leaf, bytes32[] memory proof) public pure returns (bool) {
+        bytes32 computedHash = leaf;
+        for (uint256 i = 0; i < proof.length; i++) {
+            if (computedHash < proof[i])
+                computedHash = keccak256(abi.encodePacked(computedHash, proof[i]));
+            else
+                computedHash = keccak256(abi.encodePacked(proof[i], computedHash));
+        }
+        return computedHash==root;
+    }
+    function ChannelTimeout() public {
+        require(now >= startDate + channelTimeout);
+        selfdestruct(channelSender);
+    }
+}
